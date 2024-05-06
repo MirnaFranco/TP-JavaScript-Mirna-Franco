811db94cacf98f2a4d2589bfd961fe0e9fbe88ee
Mirna <mirnafranco58@gmail.com>
2024-05-06 09:57:07 -0300

    se termino diaSemana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,26 @@
+const numero = parseInt(prompt("Ingrese un número del 1 al 7:"));
+switch (numero) {
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miércoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("Error: El número ingresado no corresponde a un día de la semana.");
+}
--- assets/js/parlmpar.js
+++ assets/js/parlmpar.js
@@ -1,4 +1,4 @@
-const numero = parseInt(prompt("Por favor, ingresa un número:"));
+const numero = parseInt(prompt("Ingrese un número:"));
 if (numero % 2 === 0) {
     console.log("El número ingresado es par");
 } else {
