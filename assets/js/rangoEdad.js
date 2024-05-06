7828bdad6b0b44f5327e3749920ab56ec796e444
Mirna <mirnafranco58@gmail.com>
2024-05-06 09:18:55 -0300

    se termino rangoEdad

---
+++ assets/js/rangoEdad.js
@@ -0,0 +1,11 @@
+let edad = prompt("Ingrese su edad")
+console.log(edad)
+if (edad >= 0 && edad <=12){
+    console.log("Eres un niño")
+}else if(edad >=13 && edad <=19)
+    console.log("Eres un adolescente")
+    if (edad >=20 && edad <=59){
+        console.log("Eres un adulto")
+    }else if(edad >=60)
+        console.log("Eres un adulto mayor")
+        
