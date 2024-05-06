5b77b158e0cb8c1ad0f9fd3a8e2844f55930295c
Mirna <mirnafranco58@gmail.com>
2024-05-06 09:29:51 -0300

    se termino parImpar

---
+++ assets/js/parlmpar.js
@@ -0,0 +1,6 @@
+const numero = parseInt(prompt("Por favor, ingresa un número:"));
+if (numero % 2 === 0) {
+    console.log("El número ingresado es par");
+} else {
+    console.log("El número ingresado es impar");
+}
